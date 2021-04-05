ee323a08e3945431e68ec93425634a3c7523e262
Vishaal Meduri <vishaal.meduri@gmail.com>
2021-04-05 15:31:30 -0500

    Pug and Routes for Login

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const { User } = require('../db/models')
 const { csrfProtection, asyncHandler } = require('./utils');
+const bcrypt = require('bcryptjs');
+
 
 router.get('/sign-up', csrfProtection, asyncHandler(async (req, res) => {
   const user = User.build();
@@ -71,6 +73,8 @@ router.post('/sign-up', csrfProtection, userValidators, asyncHandler(async (req,
   const validatorErrors = validationResult(req);
 
   if (validatorErrors.isEmpty()) {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    user.hashed_password = hashedPassword;
     await user.save();
     res.redirect('/');
   } else {
@@ -84,6 +88,46 @@ router.post('/sign-up', csrfProtection, userValidators, asyncHandler(async (req,
   }
 
 }));
+
+router.get('/login', csrfProtection, (req, res) => {
+  res.render('login', {
+    title: "Login",
+    csrfToken: req.csrfToken(),
+  })
+});
+
+const loginValidators = [
+  check('emailAddress')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Email Address'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Password'),
+];
+
+router.post('/user/login', csrfProtection, loginValidators,
+  asyncHandler(async (req, res) => {
+    const {
+      email,
+      password,
+    } = req.body;
+
+    let errors = [];
+    const validatorErrors = validationResult(req);
+
+    if (validatorErrors.isEmpty()) {
+      // TODO Attempt to login the user.
+    } else {
+      errors = validatorErrors.array().map((error) => error.msg);
+    }
+
+    res.render('login', {
+      title: 'Login',
+      email,
+      errors,
+      csrfToken: req.csrfToken(),
+    });
+  }));
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
