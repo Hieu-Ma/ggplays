ab00dd59213bef77e4f928793d242a153af56bf0
Hieu <simshieu1@gmail.com>
2021-04-05 11:07:11 -0700

    Adding pug files

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,50 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const db = require('../db/models');
+const {csrfProtection, asyncHandler} = require('./utils');
+
+router.get('/sign-up', csrfProtection, (req, res) => {
+  const user = db.User.build();
+  res.render('sign-up', {
+    title: 'Sign Up',
+    user,
+    csrfToken: req.csrfToken(),
+  });
+});
+
+const userValidators = [
+
+];
+
+router.post('/sign-up', csrfProtection, userValidators, asyncHandler(async (req, res) => {
+  const {
+    username,
+    email,
+    password,
+  } = req.body;
+
+  const user = db.User.build({
+    username,
+    email,
+  });
+
+  const validatorErrors = validationResult(req);
+
+  if (validatorErrors.isEmpty()) {
+    await user.save();
+    res.redirect('/');
+  } else {
+    const errors = validatorErrors.array().map((error) => error.msg);
+    res.render('sign-up', {
+      title: 'Sign Up',
+      user,
+      errors,
+      csrfToken: req.csrfToken(),
+    });
+  }
+
+}));
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
