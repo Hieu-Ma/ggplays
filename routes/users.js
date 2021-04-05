18d1ea039110247f92387c7cc12920c125cd2de1
Hieu <simshieu1@gmail.com>
2021-04-05 14:17:21 -0700

    Persisting user login state

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@ const { check, validationResult } = require('express-validator');
 const { User } = require('../db/models')
 const { csrfProtection, asyncHandler } = require('./utils');
 const bcrypt = require('bcryptjs');
+const db = require('../db/models');
 
 
 router.get('/sign-up', csrfProtection, asyncHandler(async (req, res) => {
@@ -33,6 +34,8 @@ const userValidators = [
     .withMessage('Please provide a valid email')
     .isLength({ max: 30 })
     .withMessage('Email must not be more than 30 characters')
+    .isEmail({checkFalsy: true})
+    .withMessage('Input must be a valid email')
     .custom(value => {
       return User.findOne({ where: { email: value } }).then(user => {
         if (user) {
@@ -97,7 +100,7 @@ router.get('/login', csrfProtection, (req, res) => {
 });
 
 const loginValidators = [
-  check('emailAddress')
+  check('email')
     .exists({ checkFalsy: true })
     .withMessage('Please provide a value for Email Address'),
   check('password')
@@ -105,18 +108,35 @@ const loginValidators = [
     .withMessage('Please provide a value for Password'),
 ];
 
-router.post('/user/login', csrfProtection, loginValidators,
+router.post('/login', csrfProtection, loginValidators,
   asyncHandler(async (req, res) => {
     const {
       email,
       password,
     } = req.body;
 
+    console.log(req.body);
+
     let errors = [];
     const validatorErrors = validationResult(req);
 
     if (validatorErrors.isEmpty()) {
-      // TODO Attempt to login the user.
+      const user = await User.findOne({ where: {email: email}});
+
+      if (user !== null) {
+        // If the user exists then compare their password
+        // to the provided password.
+        const passwordMatch = await bcrypt.compare(password, user.hashed_password.toString());
+
+        if (passwordMatch) {
+          // If the password hashes match, then login the user
+          // and redirect them to the default route.
+          return res.redirect('/');
+        }
+      }
+
+      errors.push('Login failed for the provided email address and password');
+
     } else {
       errors = validatorErrors.array().map((error) => error.msg);
     }
