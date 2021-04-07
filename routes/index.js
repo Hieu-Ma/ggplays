70e4160c17cbc1dd91d062bb8c8af97db2c98fa4
Vishaal Meduri <vishaal.meduri@gmail.com>
2021-04-07 11:28:23 -0500

    Added queries for FPS games

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const { Genre, Game } = require('../db/models');
+const { requireAuth } = require('../auth');
+const { asyncHandler } = require('./utils');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
-});
+router.get('/', asyncHandler(async (req, res, next) => {
+  const fps = await Game.findAll({
+    where: {
+      genre_id: 1
+    }
+  })
+  console.log("checking game", fps);
+  res.render('index', { fps });
+
+}));
 
 module.exports = router;
