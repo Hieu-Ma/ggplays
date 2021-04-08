a183f5de2a2582505d40ea2d8e5e613175c06117
Ellen Park <paarkellen@gmail.com>
2021-04-08 16:09:48 -0400

    working on apis

---
+++ public/javascripts/updatingGameshelf.js
@@ -0,0 +1,9 @@
+const gameshelfSelect = document.getElementById("select-options");
+
+document.addEventListener('change', () =>{
+    
+})
+
+// q for if game is associated with game shelf
+    // if so, delete game shel
+    // then create with new game shelf id 
