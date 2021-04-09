215fc12936db3d88d4e924f2ff3e2100de2f231c
Ellen Park <paarkellen@gmail.com>
2021-04-09 09:58:47 -0400

    added queries for gneres and games

--- routes/games-list.js
+++ routes/games-list.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator');
 const { Gameshelf, Game, User, Genre } = require('../db/models');
 const { asyncHandler } = require('./utils');
 
 router.get('/', asyncHandler(async(req, res, next) => {
-    const games = await Genre.findAll({
-        include: [Game]
-    })
+    const games = await Game.findAll()
 
-    res.render('games-list', { games })
+    const genres = await Genre.findAll()
+
+    res.render('games-list', { genres, games })
 }));
 
 
