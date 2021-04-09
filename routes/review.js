673b06d3c0fec6446b57b17fc8cfdff1928c562b
Hieu <simshieu1@gmail.com>
2021-04-09 08:36:49 -0700

    working on review page

--- routes/review.js
+++ routes/review.js
@@ -3,3 +3,10 @@ var router = express.Router();
 const { User, Game, Review, Genre, Pro, Con, Gameshelf } = require('../db/models');
 const { csrfProtection, asyncHandler } = require('./utils');
 const { loginUser, requireAuth } = require('../auth');
+
+router.get('/:id/review', asyncHandler(async(req, res) => {
+   res.render('review');
+}));
+
+
+module.exports = router;
